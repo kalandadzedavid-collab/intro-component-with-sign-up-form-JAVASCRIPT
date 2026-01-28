b1eeb395976900bed4da79cb6aa7ca0903d9bd12
student <student@local.emis.ge>
2026-01-28 18:55:51 +0400

    first part

---
+++ script.js
@@ -0,0 +1,14 @@
+const errorImg = document.querySelectorAll(".errorimg");
+const errorText = document.querySelectorAll(".errortext");
+
+const firstNameText = document.querySelector("#firstname").value;
+const firstName = document.querySelector("#firstname");
+
+const lastNameText = document.querySelector("#lastname").value;
+const lastName = document.querySelector("#lastname");
+
+const emailText = document.querySelector("#email").value;
+const email = document.querySelector("#email");
+
+const passwordText = document.querySelector("#password").value;
+const password = document.querySelector("#password");
